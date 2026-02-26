0b9979367788ed820ada237fc56ae265a4ad9a7c
Jade Manickam Reddi <jreddi4u@gmail.com>
2026-02-26 08:58:06 +0200

    StopAndGo Version 1

---
+++ src/main/java/com/mycompany/stopandgo/StopAndGo.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.stopandgo;
+
+/**
+ *
+ * @author jredd
+ */
+public class StopAndGo {
+
+    public static void main(String[] args) {
+        
+        /* A simply configured application that displays figures of stop signs
+        and go signs designed differently with each pair. */
+        
+        System.out.println("  ____________________________ ");
+        System.out.println("                               ");
+        System.out.println("    ********        ********   ");
+        System.out.println("   *        *      *        *  ");
+        System.out.println("  *   STOP!  *    *    GO!   * ");
+        System.out.println("   *        *      *        *  ");
+        System.out.println("    ********        ********   ");
+        System.out.println("       **              **      ");
+        System.out.println("       **              **      ");
+        System.out.println("       **              **      ");
+        System.out.println("       **              **      ");
+        System.out.println("      ****            ****     ");
+        System.out.println("   **********      **********  ");
+        System.out.println("  ____________________________ ");
+        
+// these two signs are made up of asterisks and have a hexagonal shape.
+        
+    
+    }
+}
